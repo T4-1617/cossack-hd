dc634dd5ba73dabffaf1b66a19b3a9a5a30ef6ad
Cossack-HD <kazakhd@gmail.com>
2016-08-30 13:09:20 +0200

    now prevents empty input for name

--- New ConsoleApp/New ConsoleApp/Program.cs
+++ New ConsoleApp/New ConsoleApp/Program.cs
@@ -10,10 +10,21 @@ namespace New_ConsoleApp
     {
         static void Main(string[] args)
         {
+            string first_name = "";
+            string last_name = "";
+
             Console.WriteLine("Vad är ditt förnamn?");
-            string first_name = Console.ReadLine();
+            first_name = Console.ReadLine();
+
+            while (first_name == "")
+            {
+                Console.WriteLine("Du har inte matat in någon text. Vänligen, skriv ditt namn");
+                first_name = Console.ReadLine();
+            }
+
+
             Console.WriteLine("Tack! Vad heter du i efternamn?");
-            string last_name = Console.ReadLine();
+            last_name = Console.ReadLine();
 
             Console.WriteLine("Hej, " + first_name + "!");
 
