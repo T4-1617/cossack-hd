df93eed98d971b4eb4477d9098d43809659f689d
Cossack-HD <kazakhd@gmail.com>
2016-09-02 20:29:05 +0200

    Battleship Remastered: planning layout

---
+++ BattleShip Remastered/BattleShip Remastered/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip_Remastered
+{
+    class Program
+    {
+        //declare common variables here
+
+        
+
+        static void Main(string[] args)
+        {
+
+            //create game field 2D array
+
+            //create random amount of ships
+
+            //start game loop until ships are 0
+
+            //1. display field
+
+            //receive coordinates
+
+            //check coordinates
+
+            //there are living ships -> goto 1
+
+            //all ships are down -> exit loop, end game screen
+
+
+
+            
+
+        }
+    }
+}
