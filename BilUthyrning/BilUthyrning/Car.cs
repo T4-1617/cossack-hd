0571832015877843f8aeff131b5771c26b23224c
Cossack-HD <kazakhd@gmail.com>
2016-09-14 11:13:49 +0200

    BilUthyrhing: added class, arraylist, 5 hard coded cars with unique properties

---
+++ BilUthyrning/BilUthyrning/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilUthyrning
+{
+    class Car
+    {
+        public int id { get; set; } //id for internal handling
+        public string regnumb { get; set; } //registration number, important
+        public bool available { get; set; } //essential for proper functionality
+        public string model { get; set; } //essential for proper displaying
+        public string color { get; set; } //important in many cases
+        public int mileage { get; set; } //important for service
+
+        public string currentUser { get; set; } //a nice to have if you want your car back
+
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}", model, color); //returns handy strings to display in listbox
+        }
+
+
+        public void batchProperties(int newID, string newREGNUMB, bool newAVAILABLE, string newMODEL, string newCOLOR, int newMILEAGE)
+        {
+            id = newID;
+            regnumb = newREGNUMB;
+            available = newAVAILABLE;
+            model = newMODEL;
+            color = newCOLOR;
+            mileage = newMILEAGE;
+        }
+    }
+}
--- BilUthyrning/BilUthyrning/Form1.cs
+++ BilUthyrning/BilUthyrning/Form1.cs
@@ -12,9 +12,37 @@ namespace BilUthyrning
 {
     public partial class Form1 : Form
     {
+
+        System.Collections.ArrayList myCars = new System.Collections.ArrayList();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void populateDemo()
+        {
+            Car c = new Car { id = 0, regnumb="MLG420", available = true, model = "Volvo V70", color = "Black", currentUser = "None", mileage=69 };
+            myCars.Add(c);
+
+            c.batchProperties(1, "POTATIS", false, "Wolksvagen Fox", "´Light Brown", 9000);
+            myCars.Add(c);
+
+            c.batchProperties(2, "NCP360", true, "Porsche 911", "Orange", 21);
+            myCars.Add(c);
+
+            c.batchProperties(3, "NOP101", true, "Volvo XC90", "Green", 57384);
+            myCars.Add(c);
+
+            c.batchProperties(4, "UWM800", false, "Volvo XC90", "Green", 3544);
+            myCars.Add(c);
+
         }
     }
 }
